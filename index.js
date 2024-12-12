f034c052faa8f796efa38b9c60cae61f999e0e88
glintwine <a.alekseev@rigintelpro.ru>
2024-12-13 02:52:28 +0500

    commit

--- index.js
+++ index.js
@@ -142,24 +142,21 @@ async function findComponentsWithinBudget(budget) {
 }
 
 app.post('/web-data', async(req, res) => {
-    const {queryId, products, totalPrice} = req.body;
+    const {queryId, products = [], totalPrice} = req.body;
     try{
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
-            input_message_content: {message_text: 'Поздравляю с покупкой вы приобрели товар на сумму ' + totalPrice}
+            input_message_content: {
+                message_text: `Поздравляю с покупкой вы приобрели товар на сумму ${totalPrice},
+                ${products.map(item => item.title).join(', ')}`
+                }
         })
         return res.status(200).json({})
     }
     catch(error){
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Не удалось совершить покупку' +
-                '',
-            input_message_content: {message_text: 'Не удалось совершить покупку'}
-        })
+
         return res.status(500).json({})
 
     }
@@ -167,4 +164,5 @@ app.post('/web-data', async(req, res) => {
 
 const PORT = 4000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`
+            }Listening on port ${PORT}`));
